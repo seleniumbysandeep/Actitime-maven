4498710ff636e44673fc49b926bded1adff73cfd
ADMIN <ADMIN@DESKTOP-SR1I626>
2022-07-07 13:46:19 +0530

    first commit

---
+++ ActiTIMEMavenProject/src/test/java/com/actitime/testscript/CustomerModule.java
@@ -0,0 +1,18 @@
+package com.actitime.testscript;
+
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+import com.actitime.generic.BaseClass;
+
+public class CustomerModule extends BaseClass{
+
+	@Test
+	public void testCreateCutomer() {
+		Reporter.log("Create Customer",true);
+		
+		
+	}
+	
+}
